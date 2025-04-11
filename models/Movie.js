9e628ffdba74db82b06f1fa75620494d5080e7a6
Piyush Bafna <130243298+piyushdev04@users.noreply.github.com>
2025-04-11 23:57:20 +0530

    Update Movie.js

--- models/Movie.js
+++ models/Movie.js
@@ -1,13 +1,13 @@
 const mongoose = require("mongoose");
 
 const movieSchema = new mongoose.Schema({
-    title: { type: String, required: true },
-    description: String,
-    genre: String,
-    duration: Number,
-    showtimes: [String],
-    date: String,
-    availableSeats: Number
+  title: { type: String, required: true },
+  description: String,
+  genre: String,
+  duration: Number,
+  date: Date,
+  showtimes: [String],
+  availableSeats: Number
 });
 
 module.exports = mongoose.model("Movie", movieSchema);
