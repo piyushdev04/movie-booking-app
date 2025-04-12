aaea271aa50bb0f1674943dccd3bb8283da5f764
Piyush Bafna <130243298+piyushdev04@users.noreply.github.com>
2025-04-12 22:41:12 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,11 +1,13 @@
 const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config();
 
 const connectDB = async () => {
     try {
         await mongoose.connect(process.env.MONGO_URI);
         console.log("MongoDB connected");
     } catch (err) {
-        console.error(err.message);
+        console.error("MongoDB Connection Failed", err);
         process.exit(1);
     }
 };
