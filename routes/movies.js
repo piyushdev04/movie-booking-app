a6f96efbd621003631e37647e5a6e28fde507f46
Piyush Bafna <130243298+piyushdev04@users.noreply.github.com>
2025-04-12 22:41:37 +0530

    Update movies.js

--- routes/movies.js
+++ routes/movies.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const Movie = require("../models/Movie");
 
-router.get("/", async (req, res) => {
+router.get("/movies/:id", async (req, res) => {
     try {
         const movies = await Movie.find({});
         res.json(movies);
